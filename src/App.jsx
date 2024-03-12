96125addbb81f89d2f8cdbc3a77a9cd9f1bc3b8b
SalmaElMarhoumi <salmaelmarhoumi2000@gmail.com>
2024-03-13 00:44:10 +0200

    main footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './Components/HomePage/HomePage';
 import AddHotel from './Components/AddHotel/AddHotel';
 import Layout from './Components/AdminLayout/Layout';
 
 function App() {
   let routers = createBrowserRouter([
+    { path: '/', element: <HomePage /> },
     { path: '/addhotel', element: <Layout /> }
   ]);
   return <RouterProvider router={routers}/>
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../MainNavbar/Navbar'
+import Footer from '../MainFooter/Footer'
 import { Outlet } from 'react-router-dom'
 
 export default function HomePage() {
     return <>
         <Navbar />
         <Outlet/>
+        <Footer />
     </>
     
 }
